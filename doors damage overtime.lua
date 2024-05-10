893cfe455110962c72ae69bcd5c0e74d3c0c6fb8
Playr <69977963+playr-lol@users.noreply.github.com>
2024-05-10 17:18:10 -0400

    Update doors damage overtime.lua

--- doors damage overtime.lua
+++ doors damage overtime.lua
@@ -1,11 +1,24 @@
-local doorHealthGained = 7
-local itemHealthGained = 15
+                                                                                             local doorHealthGained local itemHealthGained local breakerHealthGained local tickrate local damage
 
-local tickrate = 0.1 -- how many seconds until update healthh
-local damage = 0.1 -- how much damage will it do
 
+ --                 Heal settings
+                 doorHealthGained = 7
+                 itemHealthGained = 15
+                breakerHealthGained = 30
+
+--                    Mechanic settings
+                 tickrate = 0.1 -- how many seconds until you get damaged
+                 damage = 0.1 -- how much damage will it do
+
+
+
+
+
+
+
+
+-- hi this is thy code
 if not game:IsLoaded() then game.Loaded:Wait() end
-repeat task.wait() until game:GetService("ReplicatedStorage").GameData.LatestRoom.Value ~= 0
 local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.Parent = game.CoreGui
 
@@ -17,7 +30,14 @@ TextLabel.TextSize = 48
 TextLabel.TextStrokeColor3 = Color3.new(1,1,1)
 TextLabel.TextStrokeTransparency = 0
 TextLabel.BackgroundTransparency = 1
-TextLabel.Text = ""
+TextLabel.Text = ":)"
+repeat task.wait() until game:GetService("ReplicatedStorage").GameData.LatestRoom.Value ~= 0
+TextLabel.Text = "started!"
+task.spawn(function()
+    task.wait(1)
+    TextLabel.Text = ""
+    TextLabel:Destroy()
+end)
 local hpeaten = 0
 local hptaken = 0
 local player = game.Players.LocalPlayer
@@ -72,17 +92,34 @@ player.Character.ChildAdded:Connect(function(ae)
         end
     end
 end)
-game:GetService("RunService").RenderStepped:Connect(function()
+game.ReplicatedStorage.RemotesFolder.FlashSpecify.OnClientEvent:Connect(function(thing, amount, playerId) -- thanks, damian!
+    if thing == "BreakerPoles" then
+        if player.Character and player.Character:FindFirstChildWhichIsA("Humanoid") then
+            local humanoid = player.Character:FindFirstChildWhichIsA("Humanoid")
+            if humanoid.Health > 0 then
+                if hptaken < breakerHealthGained then
+                    humanoid.Health += hptaken
+                    hptaken = 0
+                else
+                    humanoid.Health += breakerHealthGained
+                    hptaken -= breakerHealthGained
+                end
+            end
+        end
+        print(tostring(playerId).. " picked up a breaker pole! ".. tostring(amount).. " have been picked up.")
+    end
+end)
+
+game:GetService("RunService").RenderStepped:Connect(function() -- gets rid of camera shake
     if player.PlayerGui:FindFirstChild("MainUI") then task.spawn(function() require(player.PlayerGui.MainUI.Initiator.Main_Game).csgo = CFrame.new(0,0,0) end) end -- doing a task.spawn here cause if you die it breaks
 end)
 while task.wait(tickrate) do
-    if player.Character and player.Character:FindFirstChildWhichIsA("Humanoid") then
+    if player.Character and player.Character:FindFirstChildWhichIsA("Humanoid") and player.Character:FindFirstChild("Collison") and not player.Character.Collision.Anchored then
         local humanoid = player.Character:FindFirstChildWhichIsA("Humanoid")
         if humanoid.Health > 0 then
             humanoid.Health -= damage
             hptaken += damage
             hpeaten += damage
-            TextLabel.Text = (string.format("%.1f", hpeaten)).. " total HP eaten."
         end
     end
     if player.PlayerGui:FindFirstChild("MainUI") then
