021989d0aed928adeac73c815edb36bfae425053
Playr <69977963+playr-lol@users.noreply.github.com>
2026-02-23 17:14:55 -0500

    mesh support + cleanup
    who will know my face
    now I'm Logan
    https://youtube.com/clip/Ugkx25wwD0uB_DfD3-mA85jQ6-7siARRWaOR

--- parttojson.lua
+++ parttojson.lua
@@ -1,41 +1,83 @@
+local whereToCheck = game:GetService("Workspace")
+
 print("Starting to save...")
 local starttime = os.clock()
 local yay = {
-	Anchored = false,
-	Color = {0, 0, 0},
-	Transparency = 1,
-	Position = {0, 0, 0},
-	Rotation = {0, 0, 0},
-	Orientation = {0, 0, 0},
-	Size = {0, 0, 0},
-	CanCollide = false,
+    Shape = nil,
+	Anchored = nil,
+	Color = nil,
+	Transparency = nil,
+	Position = nil,
+	Rotation = nil,
+	Orientation = nil,
+	Size = nil,
+	CanCollide = nil,
 	Material = nil,
-	ClassName = "",
-	Name = "",
-	ParentName = ""
+	ClassName = nil,
+	Name = nil,
+	ParentName = nil,
+    MeshID = nil,
+    TextureID = nil,
+    Offset = nil,
+    Scale = nil,
+    --PuID = nil,
 }
 
+local function parseV3asTable(vec : Vector3)
+    local test = {
+        vec.X,
+        vec.Y,
+        vec.Z
+    }
+    return test
+end
+
+local function formatfor(v : Instance)
+    local test = table.clone(yay)
+    test.Name = v.Name
+    test.ClassName = v.ClassName
+    --test.PuID = v.Parent.Unique
+    if v:IsA("BasePart") then
+        if v:IsA("Part") then
+            test.Shape = tostring(v.Shape)
+        end
+        test.Anchored = v.Anchored
+        test.Color = {
+            v.Color.R,
+            v.Color.G,
+            v.Color.B
+        }
+        test.Transparency = v.Transparency
+        test.Position = parseV3asTable(v.Position)
+        test.Rotation = parseV3asTable(v.Rotation)
+        test.Orientation = parseV3asTable(v.Orientation)
+        test.Size = parseV3asTable(v.Size)
+        test.ParentName = v.Parent.Name
+        test.CanCollide = v.CanCollide
+        test.Material = tostring(v.Material)
+        if v:IsA("MeshPart") then
+            test.MeshID = (v.MeshId:match("%d+"))
+            test.TextureID = tonumber(v.TextureID:match("%d+"))
+            --test.Offset = parseV3asTable(v.Offset)
+            --test.Scale = parseV3asTable(v.Scale)
+        end
+    --[[elseif v:IsA("SpecialMesh") then
+        test.MeshID = (v.MeshID:match("%d+"))
+        test.TextureID = tonumber(v.TextureID:match("%d+"))
+        test.Offset = parseV3asTable(v.Offset)
+        test.Color = {
+            v.Color.R,
+            v.Color.G,
+            v.Color.B
+        }--]]
+    end
+    return test
+end
+
 local yaya = {}
-for i, v in workspace:GetDescendants() do
+for i, v in whereToCheck:GetDescendants() do
 	if v:IsA("BasePart") then
-		local test = table.clone(yay)
-		test.Anchored = v.Anchored
-		test.Color = {
-			v.Color.R,
-			v.Color.G,
-			v.Color.B
-		}
-		test.Transparency = v.Transparency
-		test.Position = {v.Position.X, v.Position.Y, v.Position.Z}
-		test.Rotation = {v.Rotation.X, v.Rotation.Y, v.Rotation.Z}
-		test.Orientation = {v.Orientation.X, v.Orientation.Y, v.Orientation.Z}
-		test.Size = {v.Size.X, v.Size.Y, v.Size.Z}
-		test.Name = v.Name
-		test.ParentName = v.Parent.Name
-		test.CanCollide = v.CanCollide
-		test.Material = tostring(v.Material)
-		test.ClassName = v.ClassName
-		table.insert(yaya, test)
+		table.insert(yaya, formatfor(v))
 	end
 end
 local name = `Parts_{game.PlaceId}.json`
